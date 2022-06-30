238025c893d848e9357b371cba5026b19b19c6c1
Pugalarasan <75373225+0xlittleboy@users.noreply.github.com>
2022-06-30 19:40:19 +0530

    Update subdomz.sh

--- subdomz.sh
+++ subdomz.sh
@@ -15,6 +15,9 @@ echo ""
 echo ""
 sleep 1
 
+mkdir -p ~/bbht/recon/
+cd ~/bbht/recon/
+
 #SubDomain Enumeration Started 
 echo "----------------------------------------"
 echo "SubDomain Enumeration started against $1"
@@ -22,40 +25,40 @@ echo "----------------------------------------"
 sleep 1
 
 #Assetfinder
-assetfinder --subs-only $1 | tee -a $1-assetfinder.txt
+assetfinder --subs-only $1 | tee -a ~/bbht/recon/$1/$1-assetfinder.txt
 
 #Findomain
-findomain-linux -t $1 --quiet | tee -a $1-findomain.txt
+findomain-linux -t $1 --quiet | tee -a ~/bbht/recon/$1/$1-findomain.txt
 
 #SubFinder
-subfinder -d $1 -silent -o $1-subfinder.txt
+subfinder -d $1 -silent -o ~/bbht/recon/$1/$1-subfinder.txt
 
 #Amass
-amass enum -passive -d $1 -config ~/.config/amass/config.ini -o $1-amass.txt
+amass enum -passive -d $1 -config ~/.config/amass/config.ini -o ~/bbht/recon/$1/$1-amass.txt
 
 #Sublist3r
-sublist3r -d $1 --quiet -o $1-sublist3r.txt
+sublist3r -d $1 --quiet -o ~/bbht/recon/$1/$1-sublist3r.txt
 
 #Censys-Subdomain_finder
-censys-subdomain-finder.py $1 -o $1-censys.txt
+censys-subdomain-finder.py $1 -o ~/bbht/recon/$1/$1-censys.txt
 
 #BufferOver.run
-curl -s https://dns.bufferover.run/dns?q=.$1.com | jq -r .FDNS_A[] | cut -d',' -f2 | sort -u | tee $1-bufferover.txt
+curl -s https://dns.bufferover.run/dns?q=.$1.com | jq -r .FDNS_A[] | cut -d',' -f2 | sort -u | tee ~/bbht/recon/$1/$1-bufferover.txt
 
 #Riddler.io
-curl -s "https://riddler.io/search/exportcsv?q=pld:target.com" | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u | tee $1-riddler.txt
+curl -s "https://riddler.io/search/exportcsv?q=pld:target.com" | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u | tee ~/bbht/recon/$1/$1-riddler.txt
 
 #CertSpotter
-curl -s "https://certspotter.com/api/v1/issuances?domain=$1&include_subdomains=true&expand=dns_names" | jq .[].dns_names | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u | tee -a $1-certspotter.txt
+curl -s "https://certspotter.com/api/v1/issuances?domain=$1&include_subdomains=true&expand=dns_names" | jq .[].dns_names | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u | tee -a ~/bbht/recon/$1/$1-certspotter.txt
 
 #Archive
-curl -s "http://web.archive.org/cdx/search/cdx?url=*.$1/*&output=text&fl=original&collapse=urlkey" | sed -e 's_https*://__' -e "s/\/.*//" | sort -u | tee -a $1-archive.txt
+curl -s "http://web.archive.org/cdx/search/cdx?url=*.$1/*&output=text&fl=original&collapse=urlkey" | sed -e 's_https*://__' -e "s/\/.*//" | sort -u | tee -a ~/bbht/recon/$1/$1-archive.txt
 
 #JLDC
-curl -s "https://jldc.me/anubis/subdomains/$1" | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u | tee -a $1-jldc.txt
+curl -s "https://jldc.me/anubis/subdomains/$1" | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u | tee -a ~/bbht/recon/$1/$1-jldc.txt
 
 #crt.sh
-curl -s "https://crt.sh/?q=%25.$1&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee -a $1-crtsh.txt
+curl -s "https://crt.sh/?q=%25.$1&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee -a ~/bbht/recon/$1/$1-crtsh.txt
 
 #Removing Duplicates
 #Sorting
@@ -65,9 +68,9 @@ echo "----------------------"
 echo "Sorting the Subdomains"
 echo "----------------------"
 echo ""
-cat $1-assetfinder.txt $1-amass.txt $1-findomain.txt $1-sublist3r.txt $1-subfinder.txt $1-bufferover.txt $1-riddler.txt $1-certspotter.txt $1-archive.txt $1-jldc.txt $1-crtsh.txt $1-censys.txt | sort -u | tee -a $1-subs.txt
+cat ~/bbht/recon/$1/$1-assetfinder.txt $1-amass.txt $1-findomain.txt $1-sublist3r.txt $1-subfinder.txt $1-bufferover.txt $1-riddler.txt $1-certspotter.txt $1-archive.txt $1-jldc.txt $1-crtsh.txt $1-censys.txt | sort -u | tee -a $1-subs.txt
 sleep 1
-rm $1-assetfinder.txt $1-amass.txt $1-findomain.txt $1-sublist3r.txt $1-subfinder.txt $1-bufferover.txt $1-riddler.txt $1-certspotter.txt $1-archive.txt $1-jldc.txt $1-crtsh.txt $1-censys.txt
+rm ~/bbht/recon/$1/$1-assetfinder.txt $1-amass.txt $1-findomain.txt $1-sublist3r.txt $1-subfinder.txt $1-bufferover.txt $1-riddler.txt $1-certspotter.txt $1-archive.txt $1-jldc.txt $1-crtsh.txt $1-censys.txt
 
 #Checking Alive Domains
 sleep 1
@@ -76,6 +79,7 @@ echo "--------------------------"
 echo "Checking Alive Subs for $1"
 echo "--------------------------"
 echo ""
+cd ~/bbht/recon/$1/
 httpx -l $1-subs.txt -p 80,443 -o $1-alive.txt
 
 sleep 1
