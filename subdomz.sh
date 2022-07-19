c068beb422f3634c393f94b658e63303f429aecc
Pugalarasan <75373225+0xlittleboy@users.noreply.github.com>
2022-07-19 09:28:28 +0530

    Update subdomz.sh

--- subdomz.sh
+++ subdomz.sh
@@ -16,15 +16,19 @@
 ##      * CTFR                          ##      * HackerTarget              ##
 ##############################################################################
 # This script is originally made by @itsbing0o and I made a changes to work more effectively
+sleep 1
 cat <<"EOF"
-               __        __                   
+               __        __
    _______  __/ /_  ____/ /___  ____ ___  ____
   / ___/ / / / __ \/ __  / __ \/ __ `__ \/_  /
  (__  ) /_/ / /_/ / /_/ / /_/ / / / / / / / /_
 /____/\__,_/_.___/\__,_/\____/_/ /_/ /_/ /___/
-                                              
-          Made with ❤️ by litt1eb0y      
-         🐦 twitter.com/0xlittleboy 
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@   All in One Subdomain Enumeration Tool    @
+@--------------------------------------------@
+@       Made with ❤️ by litt1eb0y            @
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 EOF
 sleep 1
@@ -48,9 +52,9 @@ Usage(){
 	\r# ${bold}${blue}Options${end}:
 	\r    -d, --domain       - Domain To Enumerate
 	\r    -l, --list         - List of domains
-	\r    -o, --output       - The output file to save the Final Results (Default: <TargetDomain>-DATE-TIME.txt)
-	\r    -s, --silent       - The Only output will be the found subdomains - (Results saved: subdomz-<DOMAIN>.txt).
-	\r    -p, --parallel     - To Use Parallel For Faster Results, Doesn't Work With -e/--exclude or -u/--use. 
+	\r    -o, --output       - The output file to save the Final Results
+	\r    -s, --silent       - The Only output will be the found subdomains
+	\r    -p, --parallel     - To Use Parallel For Faster Results.
 	\r    -h, --help         - Displays this help message and exit.
 
 EOF
@@ -60,7 +64,7 @@ EOF
 
 spinner(){
 	processing="${1}"
-	while true; 
+	while true;
 	do
 		dots=(
 			"/"
@@ -74,7 +78,7 @@ spinner(){
 			printf "                                    \r"
 			sleep 0.3
 		done
-		
+
 	done
 }
 
@@ -96,8 +100,8 @@ Assetfinder() {
 		[[ ${PARALLEL} == True ]] || { spinner "${bold}AssetFinder${end}" &
 			PID="$!"
 		}
-		assetfinder --subs-only $domain > tmp-assetfinder-$domain
-		kill ${PID} 2>/dev/null
+		assetfinder --subs-only $domain 1> tmp-assetfinder-$domain 2>/dev/null
+		[[ ${PARALLEL} == True ]] || kill ${PID} 2>/dev/null
 		echo -e "$bold[*] AssetFinder$end: $(echo && cat < tmp-assetfinder-$domain && echo )"
 	}
 }
@@ -108,8 +112,8 @@ Findomain() {
 		[[ ${PARALLEL} == True ]] || { spinner "${bold}Findomain${end}" &
 			PID="$!"
 		}
-		findomain -quiet -t $domain > tmp-findomain-$domain
-		kill ${PID} 2>/dev/null
+		findomain -quiet -t $domain 1> tmp-findomain-$domain 2>/dev/null
+		[[ ${PARALLEL} == True ]] || kill ${PID} 2>/dev/null
 		echo -e "$bold[*] Findomain$end: $(echo && cat < tmp-findomain-$domain && echo )"
 	}
 }
@@ -127,9 +131,129 @@ Amass() {
 }
 
 
+Gauplus() {
+  [ "$silent" == True ] &&  gauplus -t 5 -random-agent -subs $domain |  unfurl -u domains 2>/dev/null  | anew subdomz-$domain.txt || {
+    [[ ${PARALLEL} == True ]] || { spinner "${bold}Gauplus${end}" &
+      PID="$!"
+    }
+    gauplus -t 5 -random-agent -subs $domain | unfurl -u domains 1> tmp-gauplus-$domain 2>/dev/null
+    [[ ${PARALLEL} == True ]] || kill ${PID} 2>/dev/null
+    echo -e "$bold[*] Gauplus$end: $(echo && cat < tmp-gauplus-$domain && echo )"
+  }
+}
+
+
+Waybackurls() {
+  [ "$silent" == True ] &&  waybackurls $domain |  unfurl -u domains 2>/dev/null | anew subdomz-$domain.txt || {
+    [[ ${PARALLEL} == True ]] || { spinner "${bold}Waybackurls${end}" &
+      PID="$!"
+    }
+    gauplus $domain | unfurl -u domains 1> tmp-waybackurls-$domain 2>/dev/null
+    [[ ${PARALLEL} == True ]] || kill ${PID} 2>/dev/null
+    echo -e "$bold[*] Waybackurls$end: $(echo && cat < tmp-waybackurls-$domain && echo )"
+  }
+}
+
+
+Github-Subdomains() {
+  [ "$silent" == True ] && github-subdomains -d $domain -t tokens.txt | unfurl domains 2>/dev/null | anew subdomz-$domain.txt || {
+    [[ ${PARALLEL} == True ]] || { spinner "${bold}Github-Subdomains${end}" &
+      PID="$!"
+    }
+    github-subdomains -d $domain -t tokens.txt | unfurl domains 1> tmp-github-subdomains-$domain 2>/dev/null
+    [[ ${PARALLEL} == True ]] || kill ${PID} 2>/dev/null
+    echo -e "$bold[*] Github-Subdomains$end: $(echo && cat < tmp-github-subdomains-$domain && echo )"
+  }
+}
+
+
+Chrobat() {
+  [ "$silent" == True ] && chrobat -s $domain  2>/dev/null | anew subdomz-$domain.txt || {
+    [[ ${PARALLEL} == True ]] || { spinner "${bold}Chrobat${end}" &
+      PID="$!"
+    }
+    chrobat -s $domain 1> tmp-chrobat-$domain 2>/dev/null
+    [[ ${PARALLEL} == True ]] || kill ${PID} 2>/dev/null
+    echo -e "$bold[*] Chrobat$end: $(echo && cat < tmp-chrobat-$domain && echo )"
+  }
+}
+
+
+CTFR() {
+  [ "$silent" == True ] && ctfr -d $domain | unfurl domains 2>/dev/null | anew subdomz-$domain.txt || {
+    [[ ${PARALLEL} == True ]] || { spinner "${bold}CTFR${end}" &
+      PID="$!"
+    }
+    ctfr -d $domain | unfurl domains 1> tmp-ctfr-$domain 2>/dev/null
+    [[ ${PARALLEL} == True ]] || kill ${PID} 2>/dev/null
+    echo -e "$bold[*] CTFR$end: $(echo && cat < tmp-ctfr-$domain && echo )"
+  }
+}
+
+
+Cero() {
+	[ "$silent" == True ] && cero $domain | anew subdomz-$domain.txt || {
+		[[ ${PARALLEL} == True ]] || { spinner "${bold}Cero${end}" &
+			PID="$!"
+		}
+		cero $domain 1> tmp-cero-$domain 2>/dev/null
+		[[ ${PARALLEL} == True ]] || kill ${PID} 2>/dev/null
+		echo -e "$bold[*] Cero$end: $(echo && cat < tmp-cero-$domain && echo )"
+	}
+}
+
+
+Sublister() {
+  [ "$silent" == True ] && sublist3r -d $domain | unfurl domains 2>/dev/null | anew subdomz-$domain.txt || {
+    [[ ${PARALLEL} == True ]] || { spinner "${bold}Sublister${end}" &
+      PID="$!"
+    }
+    sublist3r -d $domain | unfurl domains 1> tmp-sublister-$domain 2>/dev/null
+    [[ ${PARALLEL} == True ]] || kill ${PID} 2>/dev/null
+    echo -e "$bold[*] Sublister$end: $(echo && cat < tmp-sublister-$domain && echo )"
+  }
+}
+
+
+Sudomy() {
+  [ "$silent" == True ] && subdomy -d $domain | unfurl domains 2>/dev/null | anew subdomz-$domain.txt || {
+    [[ ${PARALLEL} == True ]] || { spinner "${bold}Sudomy${end}" &
+      PID="$!"
+    }
+    subdomy -d $domain | unfurl domains 1> tmp-sudomy-$domain 2>/dev/null
+    [[ ${PARALLEL} == True ]] || kill ${PID} 2>/dev/null
+    echo -e "$bold[*] Sudomy$end: $(echo && cat < tmp-sudomy-$domain && echo )"
+  }
+}
+
+
+Shodomain() {
+  [ "$silent" == True ] && shodomain <API-KEY> $domain 2>/dev/null | anew subdomz-$domain.txt || {
+    [[ ${PARALLEL} == True ]] || { spinner "${bold}Shodomain${end}" &
+      PID="$!"
+    }
+    shodomain <API-KEY> $domain 1> tmp-shodomain-$domain 2>/dev/null
+    [[ ${PARALLEL} == True ]] || kill ${PID} 2>/dev/null
+    echo -e "$bold[*] Shodomain$end: $(echo && cat < tmp-shodomain-$domain && echo )"
+  }
+}
+
+
+Censys-Subdomain-Finder() {
+  [ "$silent" == True ] && censys-subdomain-finder.py --censys-api-id <CENSYS_API_ID> --censys-api-secret <CENSYS_API_SECRET> $domain 2>/dev/null | anew subdomz-$domain.txt || {
+    [[ ${PARALLEL} == True ]] || { spinner "${bold}Censys-Subdomain-Finder${end}" &
+      PID="$!"
+    }
+    censys-subdomain-finder.py --censys-api-id <CENSYS_API_ID> --censys-api-secret <CENSYS_API_SECRET> $domain 1> tmp-censys-subdomain-finder.py-$domain 2>/dev/null
+    [[ ${PARALLEL} == True ]] || kill ${PID} 2>/dev/null
+    echo -e "$bold[*] Censys-Subdomain-Finder$end: $(echo && cat < tmp-censys-subdomain-finder.py-$domain && echo )"
+  }
+}
+
+
 USE() {
 	for i in $lu; do
-			$i 
+			$i
 	done
 	OUT
 }
@@ -147,13 +271,13 @@ EXCLUDE() {
 }
 
 OUT(){
-	[ "$silent" == False ] && { 
+	[ "$silent" == False ] && {
 		[ -n "$1" ] && out="$1" || out="$domain-$(date +'%Y-%m-%d').txt"
 		sort -u tmp-* > $out
 		echo -e $green"[+] The Final Results:$end $(wc -l $out)"
 		[ $resolve == True ] && ALIVE "$out" "$domain"
 
-		[ $delete == True ] && rm tmp-*	
+		[ $delete == True ] && rm tmp-*
 	}
 }
 
@@ -175,54 +299,74 @@ LIST() {
 			[[ ${PARALLEL} == True ]] && {
 				spinner "Reconnaissance" &
 				PID="$!"
-				export -f Subfinder AssetFinder Findomain Amass spinner
+				export -f Subfinder Assetfinder Findomain Amass Gauplus Waybackurls Github-Subdomains Chrobat CTFR Cero Sublilster Sudomy Shodomain Censys-Subdomain-Finder spinner
 				export domain silent bold end
-				parallel ::: Subfinder AssetFinder Findomain Amass
+				parallel ::: Subfinder Assetfinder Findomain Amass Gauplus Waybackurls Github-Subdomains Chrobat CTFR Cero Sublilster Sudomy Shodomain Censys-Subdomain-Finder
 				kill ${PID}
 				OUT
 			} || {
-				Subfinder
-				Assetfinder
-				Findomain
-				Amass
+        SubFinder
+        Assetfinder
+        Findomain
+        Amass
+        Gauplus
+        Waybackurls
+        Github-Subdomains
+        Chrobat
+        CTFR
+        Cero
+        Sublister
+        Sudomy
+        Shodomain
+        Censys-Subdomain-Finder
 				OUT
 			}
 		}
-		[ $prv == "e" ] && EXCLUDE 
-		[ $prv == "u" ] && USE 
+		[ $prv == "e" ] && EXCLUDE
+		[ $prv == "u" ] && USE
 		let count+=1
 	done < $hosts
 }
 
 Main() {
 	[ $domain == False ] && [ $hosts == False ] && { echo -e $red"[-] Argument -d/--domain OR -l/--list is Required!"$end; Usage; }
-	[ $domain != False ] && { 
-		[ $use == False ] && [ $exclude == False ] && { 
+	[ $domain != False ] && {
+		[ $use == False ] && [ $exclude == False ] && {
 			[[ ${PARALLEL} == True ]] && {
 				spinner "Reconnaissance" &
 				PID="$!"
-				export -f Subfinder AssetFinder Findomain Amass spinner
+				export -f Subfinder Assetfinder Findomain Amass Gauplus Waybackurls Github-Subdomains Chrobat CTFR Cero Sublilster Sudomy Shodomain Censys-Subdomain-Finder spinner
 				export domain silent bold end
-				parallel ::: Subfinder AssetFinder Findomain Amass
+				parallel ::: Subfinder Assetfinder Findomain Amass Gauplus Waybackurls Github-Subdomains Chrobat CTFR Cero Sublilster Sudomy Shodomain Censys-Subdomain-Finder
 				kill ${PID}
 			} || {
-				Subfinder
-				Assetfinder
-				Findomain
-				Amass
+        SubFinder
+        Assetfinder
+        Findomain
+        Amass
+        Gauplus
+        Waybackurls
+        Github-Subdomains
+        Chrobat
+        CTFR
+        Cero
+        Sublister
+        Sudomy
+        Shodomain
+        Censys-Subdomain-Finder
 			}
 			[ "$out" == False ] && OUT || OUT $out
-		} || { 
-			[ $use != False ] && USE 
+		} || {
+			[ $use != False ] && USE
 			[ $exclude != False ] && EXCLUDE
 		}
 	}
-	[ "$hosts" != False ] && { 
+	[ "$hosts" != False ] && {
 		[ $use != False ] && prv=u
 		[ $exclude != False ] && prv=e
 		[ $use == False ] && [ $exclude == False ] && prv=a
 		LIST
-	 } 
+	 }
 }
 
 
@@ -238,10 +382,20 @@ thread=40
 PARALLEL=False
 
 list=(
-	Subfinder
-	Assetfinder
-	Findomain
-	Amass
+        SubFinder
+        Assetfinder
+        Findomain
+        Amass
+        Gauplus
+        Waybackurls
+        Github-Subdomains
+        Chrobat
+        CTFR
+        Cero
+        Sublister
+        Sudomy
+        Shodomain
+        Censys-Subdomain-Finder
 	)
 
 while [ -n "$1" ]; do
