9fabc77ba616146bece136982c1f8b61d56498f0
Pugalarasan <75373225+0xPugazh@users.noreply.github.com>
2023-12-28 11:54:32 +0530

    add shosubgo & censys subdomain finder

--- install.sh
+++ install.sh
@@ -99,45 +99,22 @@ Crobat() {
             printf "[+] Crobat Installed! \n"
 }
 
-CTFR() {
-            printf "                  \r"
-	    cd ~/SubDomz/tools
-            git clone https://github.com/UnaPibaGeek/ctfr.git 
-            cd ctfr
-            pip3 install -r requirements.txt 
-            sudo cp ctfr.py /usr/local/bin
-            sudo chmod +x /usr/local/bin/ctfr.py
-            printf "[+] CTFR Installed! \n"
-}
-
 Cero() {
             printf "                              \r"
             go install -v github.com/glebarez/cero@latest 
             printf "[+] Cero Installed! \n"
 }
 
-Sudomy() {
-            printf "                          \r"
-	    cd ~/SubDomz/tools
-            git clone --recursive https://github.com/screetsec/Sudomy.git 
-            cd Sudomy && python3 -m pip install -r requirements.txt 
-            sudo cp sudomy /usr/local/bin
-            printf "[+] Sudomy Installed! \n"
-}
 
-Shodomain() {
-            printf "                                    \r"
-	    cd ~/SubDomz/tools
-            git clone https://github.com/SmoZy92/Shodomain 
-            cd Shodomain && pip install -r requirements.txt 
-            sudo cp shodomain.py /usr/local/bin
-            sudo chmod +x /usr/local/bin/shodomain.py
-            printf "[+] Shodomain Installed! \n"
+shosubgo() {
+            printf "                              \r"
+            go install github.com/incogbyte/shosubgo@latest
+            printf "[+] Shosubgo Installed! \n"
 }
 
 Censys-Subdomain-Finder() {
             printf "                  \r"
-	    cd ~/SubDomz/tools
+	          cd ~/SubDomz/tools
             git clone https://github.com/christophetd/censys-subdomain-finder.git 
             cd censys-subdomain-finder
             python3 -m venv venv
@@ -148,12 +125,6 @@ Censys-Subdomain-Finder() {
             printf "[+] Censys-Subdomain-Finder Installed! \n"
 }
 
-Httpx() {
-	         printf "                                \r"
-	         go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest 
-	         printf "[+] Httpx Installed! \n"
-}
-
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
@@ -161,8 +132,6 @@ export PATH=$PATH:/usr/local/go/bin
 sudo cp ~/go/bin/* /usr/local/bin
 
 hash go 2>/dev/null && printf "[!] Golang is already installed.\n" || { printf "[+] Installing Golang!" && Golang; }
-
-#Checking Dependencies
 
 #Checking Tools
 hash findomain 2>/dev/null && printf "[!] Findomain is already installed.\n" || { printf "[+] Installing Findomain!" && Findomain; }
@@ -176,12 +145,9 @@ hash waybackurls 2>/dev/null && printf "[!] Waybackurls is already installed.\n"
 hash github-subdomains 2>/dev/null && printf "[!] Github-Subdomains is already installed.\n" || { printf "[+] Installing Github-subdomains!" && Github-Subdomains; }
 hash gitlab-subdomains 2>/dev/null && printf "[!] Gitlab-Subdomains is already installed.\n" || { printf "[+] Installing Gitlab-subdomains!" && Gitlab-Subdomains; }
 hash crobat 2>/dev/null && printf "[!] Crobat is already installed.\n" || { printf "[+] Installing Crobat!" && Crobat; }
-hash ctfr.py 2>/dev/null && printf "[!] CTFR is already installed.\n" || { printf "[+] Installing CTFR!" && CTFR; }
 hash cero 2>/dev/null && printf "[!] Cero is already installed.\n" || { printf "[+] Installing Cero!" && Cero; }
-hash sudomy 2>/dev/null && printf "[!] Sudomy is already installed.\n" || { printf "[+] Installing Sudomy!" && Sudomy; }
-hash shodomain.py 2>/dev/null && printf "[!] Shodomain is already installed.\n" || { printf "[+] Installing Shodomain!" && Shodomain; }
 hash censys-subdomain-finder.py 2>/dev/null && printf "[!] Censys-Subdomain-Finder is already installed.\n" || { printf "[+] Installing Censys-Subdomain-Finder!" && Censys-Subdomain-Finder; }
-hash httpx 2>/dev/null && printf "[!] Httpx is already installed.\n" || { printf "[+] Installing Httpx!" && Httpx; }
+hash shosubgo 2>/dev/null && printf "[!] Shosubgo is already installed.\n" || { printf "[+] Installing Shosubgo!" && shosubgo; }
 
 
 
@@ -197,12 +163,9 @@ list=(
   	Github-Subdomains
     Gitlab-Subdomains
   	Crobat
-  	CTFR
-  	Cero
-  	Sudomy
-  	Shodomain
+    Cero
   	Censys-Subdomain-Finder
-  	Httpx
+  	shosubgo
 	)
 
       r="\e[31m"
