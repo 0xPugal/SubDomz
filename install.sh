2728a7b44a645b3040706d26ba5bdd4c5bb01a4b
0xPugazh <pugalarasan.k@outlook.com>
2024-03-20 13:44:33 +0530

    Updated

--- install.sh
+++ install.sh
@@ -1,178 +1,51 @@
-#!/bin/bash
+#+/bin/bash
+
+mkdir tools
 
 echo -e "Installation Started..."
 echo " "
-mkdir -p tools
-
-
-Golang() {
-	printf "                                \r"
-	sys=$(uname -m)
-	LATEST=$(curl -s 'https://go.dev/VERSION?m=text')
-	[ $sys == "x86_64" ] && wget https://golang.org/dl/$LATEST.linux-amd64.tar.gz -O golang.tar.gz  || wget https://golang.org/dl/$LATEST.linux-386.tar.gz -O golang.tar.gz 
-	sudo tar -C /usr/local -xzf golang.tar.gz 
-	echo "export GOROOT=/usr/local/go" >> $HOME/.bashrc
-	echo "export GOPATH=$HOME/go" >> $HOME/.bashrc
-	echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> $HOME/.bashrc
-  echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.profile
-
-	printf "[+] Golang Installed !.\n"
-}
-sleep 0.5
-
-
-sudo apt install python3-shodan -y 
-sudo apt install python3-censys -y 
-sudo apt install nmap -y 
 sudo apt install jq -y 
-sudo apt install git -y 
 sudo apt install python3 -y 
-sudo apt install python -y 
 sudo apt-get -y install python3-pip 
 sudo apt install -y parallel 
+pip3 install censys
+pip3 install shodan
 
-# Installing Tools
-Subfinder() {
-            printf "                                \r"
-            GO111MODULE=on go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest 
-            printf "[+] Subfinder Installed! \n"
-}
+GO111MODULE=on go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest 
+go install -v github.com/tomnomnom/assetfinder@latest 
+go install -v github.com/projectdiscovery/chaos-client/cmd/chaos@latest
+GO111MODULE=on go install -v github.com/OWASP/Amass/v3/...@latest 
+go install github.com/lc/gau@latest 
+go install -v github.com/gwen001/github-subdomains@latest 
+go install github.com/gwen001/gitlab-subdomains@latest
+go install github.com/cgboal/sonarsearch/cmd/crobat@latest 
+go install -v github.com/glebarez/cero@latest 
+go install github.com/incogbyte/shosubgo@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 
-Assetfinder() {
-            printf "                                \r"
-            go install -v github.com/tomnomnom/assetfinder@latest 
-            printf "[+] Assetfinder Installed! \n"
-}
+wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux.zip  && unzip findomain-linux.zip 
+chmod +x findomain
+sudo mv findomain /usr/local/bin/findomain;
 
-Chaos() {
-            printf "                  \r"
-            go install -v github.com/projectdiscovery/chaos-client/cmd/chaos@latest
-            printf "[+] Chaos Installed! \n"
-}
-
-Shuffledns() {
-            printf "                  \r"
-            go install -v github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest
-            printf "[+] Shuffledns Installed! \n"
-}
-
-Findomain() {
-	    cd ~/SubDomz/tools
-            curl -LO https://github.com/findomain/findomain/releases/latest/download/findomain-linux.zip  && unzip findomain-linux.zip 
-            chmod +x findomain
-            sudo mv findomain /usr/local/bin/findomain;
-            printf "[+] Findomain Installed! \n"
-}
-
-Amass() {
-            printf "                                \r"
-	    GO111MODULE=on go install -v github.com/OWASP/Amass/v3/...@latest 
-	    printf "[+] Amass Installed! \n"
-}
-
-Gau() {
-            printf "                                \r"
-            go install github.com/lc/gau@latest 
-            printf "[+] Gau Installed! \n"
-}
-
-Waybackurls() {
-            printf "                \r"
-            go install github.com/tomnomnom/waybackurls@latest 
-            printf "[+] Waybackurls Installed! \n"
-}
-
-Github-Subdomains() {
-            printf "            \r"
-            go install -v github.com/gwen001/github-subdomains@latest 
-            printf "[+] Github-Subdomains Installed! \n"
-}
-
-Gitlab-Subdomains() {
-            printf "          \r"
-            go install github.com/gwen001/gitlab-subdomains@latest
-            printf "[+] Gitlab-Subdomains Installed! \n"
-}
-Crobat() {
-            printf "                  \r"
-            go install github.com/cgboal/sonarsearch/cmd/crobat@latest 
-            printf "[+] Crobat Installed! \n"
-}
-
-Cero() {
-            printf "                              \r"
-            go install -v github.com/glebarez/cero@latest 
-            printf "[+] Cero Installed! \n"
-}
-
-
-shosubgo() {
-            printf "                              \r"
-            go install github.com/incogbyte/shosubgo@latest
-            printf "[+] Shosubgo Installed! \n"
-}
-
-Censys-Subdomain-Finder() {
-            printf "                  \r"
-	          cd ~/SubDomz/tools
-            git clone https://github.com/christophetd/censys-subdomain-finder.git 
-            cd censys-subdomain-finder
-            python3 -m venv venv
-            source venv/bin/activate
-            pip3 install -r requirements.txt 
-            sudo ln -svf censys-subdomain-finder.py /usr/bin/censys-subdomain-finder.py 
-            sudo chmod +x /usr/bin/censy-subdomain-finder.py 
-            printf "[+] Censys-Subdomain-Finder Installed! \n"
-}
-
-export GOROOT=/usr/local/go
-export GOPATH=$HOME/go
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-export PATH=$PATH:/usr/local/go/bin
-sudo cp ~/go/bin/* /usr/local/bin
-
-hash go 2>/dev/null && printf "[!] Golang is already installed.\n" || { printf "[+] Installing Golang!" && Golang; }
+echo ""
 
 #Checking Tools
-hash findomain 2>/dev/null && printf "[!] Findomain is already installed.\n" || { printf "[+] Installing Findomain!" && Findomain; }
-hash subfinder 2>/dev/null && printf "[!] Subfinder is already installed.\n" || { printf "[+] Installing subfinder!" && Subfinder; }
-hash amass 2>/dev/null && printf "[!] Amass is already installed.\n" || { printf "[+] Installing Amass!" && Amass; }
-hash assetfinder 2>/dev/null && printf "[!] Assetfinder is already installed.\n" || { printf "[+] Installing Assetfinder!" && Assetfinder; }
-hash chaos 2>/dev/null && printf "[!] Chaos is already installed.\n" || { printf "[+] Installing Chaos!" && Chaos; }
-hash shuffledns 2>/dev/null && printf "[!] Shuffledns is already installed.\n" || { printf "[+] Installing Shuffledns!" && Shuffledns; }
-hash gau 2>/dev/null && printf "[!] Gau is already installed.\n" || { printf "[+] Installing Gau!" && Gau; }
-hash waybackurls 2>/dev/null && printf "[!] Waybackurls is already installed.\n" || { printf "[+] Installing Waybackurls!" && Waybackurls; }
-hash github-subdomains 2>/dev/null && printf "[!] Github-Subdomains is already installed.\n" || { printf "[+] Installing Github-subdomains!" && Github-Subdomains; }
-hash gitlab-subdomains 2>/dev/null && printf "[!] Gitlab-Subdomains is already installed.\n" || { printf "[+] Installing Gitlab-subdomains!" && Gitlab-Subdomains; }
-hash crobat 2>/dev/null && printf "[!] Crobat is already installed.\n" || { printf "[+] Installing Crobat!" && Crobat; }
-hash cero 2>/dev/null && printf "[!] Cero is already installed.\n" || { printf "[+] Installing Cero!" && Cero; }
-hash censys-subdomain-finder.py 2>/dev/null && printf "[!] Censys-Subdomain-Finder is already installed.\n" || { printf "[+] Installing Censys-Subdomain-Finder!" && Censys-Subdomain-Finder; }
-hash shosubgo 2>/dev/null && printf "[!] Shosubgo is already installed.\n" || { printf "[+] Installing Shosubgo!" && shosubgo; }
+hash findomain 2>/dev/null && printf "[+] Findomain - Installed\n" || printf "[-] Findomain - Not installed\n" 
+hash subfinder 2>/dev/null && printf "[+] Subfinder - Installed.\n" ||printf "[-] Subfinder - Not installed\n" 
+hash amass 2>/dev/null && printf "[+] Amass - Installed.\n" || printf "[-] Amass - Not installed\n" 
+hash assetfinder 2>/dev/null && printf "[+] Assetfinder - Installed.\n" || printf "[-] Assetfinder - Not installed\n" 
+hash chaos 2>/dev/null && printf "[+] Chaos - Installed.\n" || printf "[-] Chaos - Not installed\n" 
+hash gau 2>/dev/null && printf "[+] Gau - Installed.\n" || printf "[-] Gau - Not installed\n" 
+hash waybackurls 2>/dev/null && printf "[+] Waybackurls - Installed.\n" || printf "[-] Waybackurls - Not installed\n" 
+hash github-subdomains 2>/dev/null && printf "[+] Github-Subdomains - Installed.\n" || printf "[-] Github-Subdomains - Not installed\n" 
+hash gitlab-subdomains 2>/dev/null && printf "[+] Gitlab-Subdomains - Installed.\n" || printf "[-] Gitlab-Subdomains - Not installed\n" 
+hash crobat 2>/dev/null && printf "[+] Crobat - Installed.\n" || printf "[-] Crobat - Not installed\n" 
+hash cero 2>/dev/null && printf "[+] Cero - Installed.\n" || printf "[-] Cero - Not installed\n" 
+hash shosubgo 2>/dev/null && printf "[+] Shosubgo - Installed.\n" || printf "[-] Shosubgo - Not installed\n" 
+hash censys 2>/dev/null && printf "[+] censys - Installed.\n" || printf "[-] censys - Not installed\n" 
 
-
-
-list=(
-	  Golang
-	  Subfinder
-	  Assetfinder
-    Chaos
-	  Findomain
-	  Amass
-  	Gau
-  	Waybackurls
-  	Github-Subdomains
-    Gitlab-Subdomains
-  	Crobat
-    Cero
-  	Censys-Subdomain-Finder
-  	shosubgo
-	)
-
-      r="\e[31m"
-      g="\e[32m"
-      e="\e[0m"
-
-for prg in ${list[@]}
-do
-      hash $prg 2>/dev/null && printf "[$prg]$g Done$e\n" || printf "[$prg]$r Not Installed! Check Again.$e\n"
-    done
+echo """
++ Manually add the censys api key and secret key manually using 'censys config'
++ Add the API keys and tokens in Config.txt file
++ Feed the API keys and tokens in subfinder and amass config file for more subdomains
+"""
