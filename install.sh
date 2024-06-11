5353aab938a986a1091cdfc8e2acb38973e69262
Pugalarasan <75373225+0xPugazh@users.noreply.github.com>
2024-06-11 20:47:55 +0530

    Update 3.0

--- install.sh
+++ install.sh
@@ -1,159 +1,27 @@
 #!/bin/bash
-#
-Parallel(){
-    printf "                                \r"
-        sudo apt-get install parallel -y
-}
-
-JQ() {
-    printf "                                \r"
-        sudo apt-get install jq -y
-}
-
-Python() {
-    printf "                                \r"
-        sudo apt-get install python3 -y
-}
-
-Pip() {
-    printf "                                \r"
-        sudo apt-get install python3-pip -y
-}
-
-Shodan() {
-    printf "                                \r"
-    pip3 install shodan || pip3 install shodan --break-system-packages
-}
-
-Censys() {
-    printf "                                \r"
-    pip3 install censys || pip3 install censys --break-system-packages
-}
-
-Subfinder() {
-        printf "                                \r"
-        go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-}
-
-Amass() {
-        printf "                                \r"
-        go install -v github.com/owasp-amass/amass/v3/...@master
-}
-
-Assetfinder() {
-        printf "                                \r"
-        go install github.com/tomnomnom/assetfinder@latest
-}
-
-Chaos() {
-        printf "                                \r"
-        go install -v github.com/projectdiscovery/chaos-client/cmd/chaos@latest
-}
-
-Findomain() {
-        printf "                                \r"
-        wget https://github.com/Findomain/Findomain/releases/download/8.2.1/findomain-linux.zip
-        unzip findomain-linux.zip
-        rm findomain-linux.zip
-        chmod +x findomain
-        sudo mv findomain /usr/bin/
-}
-
-Haktrails() {
-        printf "                                \r"
-        go install -v github.com/hakluke/haktrails@latest
-}
-
-Gau() {
-        printf "                                \r"
-        go install github.com/lc/gau/v2/cmd/gau@latest
-        wget ~/ https://raw.githubusercontent.com/lc/gau/master/.gau.toml
-}
-
-Github-subdomains() {
-        printf "                                \r"
-        go install github.com/gwen001/github-subdomains@latest
-}
-
-Gitlab-subdomains() {
-        printf "                                \r"
-        go install github.com/gwen001/gitlab-subdomains@latest
-}
-
-Cero() {
-        printf "                                \r"
-        go install -v github.com/glebarez/cero@latest
-}
-
-Shosubgo() {
-        printf "                                \r"
-        go install github.com/incogbyte/shosubgo@latest
-}
-
-Httpx() {
-        printf "                                \r"
-        go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-}
-
-Anew() {
-        printf "                                \r"
-        go install -v github.com/tomnomnom/anew@latest
-}
-
-Unfurl() {
-        printf "                                \r"
-        go install github.com/tomnomnom/unfurl@latest
-}
-
-Parallel
-JQ
-Python
-Pip
-Shodan
-Censys
-Subfinder
-Amass
-Assetfinder
-Chaos
-Findomain
-Haktrails
-Gau
-Github-subdomains
-Gitlab-subdomains
-Cero
-Shosubgo
-Httpx
-Anew
-Unfurl
-
-list=(
-        parallel
-        jq
-        python3
-        pip
-        shodan
-        censys
-        subfinder
-        amass
-    assetfinder
-    chaos
-    findomain
-    haktrails
-    gau
-    github-subdomains
-    gitlab-subdomains
-    cero
-    shosubgo
-    httpx
-    anew
-    unfurl
-        )
-
-r="\e[31m"
-g="\e[32m"
-e="\e[0m"
-
-for prg in ${list[@]}
-do
-        hash $prg 2>/dev/null && printf "[$prg]$g Installed$e\n" || printf "[$prg]$r Install Manually.$e\n"
-done
+# Installing tools
+sudo apt-get install parallel -y
+sudo apt-get install jq -y
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+pip3 install shodan --break-system-packages
+pip3 install censys --break-system-packages
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/owasp-amass/amass/v3/...@master
+go install github.com/tomnomnom/assetfinder@latest
+go install -v github.com/projectdiscovery/chaos-client/cmd/chaos@latest
+wget https://github.com/Findomain/Findomain/releases/download/8.2.1/findomain-linux.zip && unzip findomain-linux.zip && rm findomain-linux.zip && chmod +x findomain && sudo mv findomain /usr/bin/
+go install -v github.com/hakluke/haktrails@latest
+go install github.com/lc/gau/v2/cmd/gau@latest && wget ~/ https://raw.githubusercontent.com/lc/gau/master/.gau.toml
+go install github.com/gwen001/github-subdomains@latest
+go install github.com/gwen001/gitlab-subdomains@latest
+go install -v github.com/glebarez/cero@latest
+go install github.com/incogbyte/shosubgo@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/tomnomnom/anew@latest
+go install github.com/tomnomnom/unfurl@latest
+git clone https://github.com/blechschmidt/massdns.git && cd massdns && make && sudo make install
+go install github.com/d3mondev/puredns/v2@latest
+go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+git clone https://github.com/trickest/resolvers
+wget https://wordlists-cdn.assetnote.io/data/manual/best-dns-wordlist.txt
